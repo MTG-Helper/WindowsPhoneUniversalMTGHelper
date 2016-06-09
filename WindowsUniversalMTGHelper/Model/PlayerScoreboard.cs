57337a0b5bd524c9189ad8d02cdeaf6ac968dab3
Lucas Alan Silvestri <silvestrilucas@outlook.com>
2016-06-09 13:40:07 -0300

    add PlayerScoreBoard Class and Tests.

---
+++ WindowsUniversalMTGHelper/Model/PlayerScoreboard.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsUniversalMTGHelper.Model
+{
+    public class PlayerScoreboard
+    {
+
+        string playerName;
+        int lifePoints;
+        int poisonPoints;
+
+        public PlayerScoreboard()
+        {
+            this.playerName = "Foo";
+            this.lifePoints = 20;
+            this.poisonPoints = 0;
+        }
+
+
+        public int getLifePoints()
+        {
+            return this.lifePoints;
+        }
+
+        public void addOneLifePoints()
+        {
+            this.lifePoints++;
+        }
+
+        public void subOneLifePoints()
+        {
+            if (this.getLifePoints() > 0)
+            {
+                this.lifePoints--;
+            }
+        }
+
+        private void addXLifePoints(int c)
+        {
+            for(int i = 0; i < c; i++)
+            {
+                this.addOneLifePoints();
+            }
+        }
+
+        private void subXLifePoints(int c)
+        {
+            for (int i = 0; i < c; i++)
+            {
+                this.subOneLifePoints();
+            }
+        }
+
+        public void addFiveLifePoints()
+        {
+            this.addXLifePoints(5);
+        }
+
+        public void subFiveLifePoints()
+        {
+            this.subXLifePoints(5);
+        }
+
+        public int getPoisonPoints()
+        {
+            return this.poisonPoints;
+        }
+
+        public void addOnePoisonPoint()
+        {
+            this.poisonPoints++;
+        }
+
+        public void subOnePoisonPoint()
+        {
+            if(this.getPoisonPoints() > 0)
+            {
+                this.poisonPoints--;
+            }
+        }
+
+        public string getPlayerName()
+        {
+            return this.playerName;
+        }
+
+        public void changeName(string aName)
+        {
+            this.playerName = aName;
+        }
+
+    }
+}
---
+++ WindowsUniversalMTGHelperTests/PlayerScoreboardTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using WindowsUniversalMTGHelper.Model;
+
+namespace WindowsUniversalMTGHelperTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCuandoLePreguntoSusPuntosDeVidaResponde20()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            Assert.AreEqual(20, sut.getLifePoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon20PuntosDeVidaYLeSumoUnaVidaCuandoLePreguntoSusPuntosDeVidaREsponde21()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.addOneLifePoints();
+            Assert.AreEqual(21, sut.getLifePoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon20PuntosDeVidaYLeRestoUnaVidaCuandoLePreguntoSusPuntosDeVidaREsponde19()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.subOneLifePoints();
+            Assert.AreEqual(19, sut.getLifePoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon20PuntosDeVidaYLeSumo5PuntosDeVidaCuandoLePreguntoSusPuntosDeVidaREsponde25()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.addFiveLifePoints();
+            Assert.AreEqual(25, sut.getLifePoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon20PuntosDeVidaYLeResto5PuntosDeVidaCuandoLePreguntoSusPuntosDeVidaREsponde25()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.subFiveLifePoints();
+            Assert.AreEqual(15, sut.getLifePoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon0PuntosDeVenenoCuandoLePreguntoSusPuntosDeVidaResponde0()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            Assert.AreEqual(0, sut.getPoisonPoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon0PuntosDeVenenoYLeSumoUnPuntoDeVenenoCuandoLePreguntoSusPuntosDeVidaResponde1()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.addOnePoisonPoint();
+            Assert.AreEqual(1, sut.getPoisonPoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardCon0PuntosDeVenenoYLeRestoUnPuntoDeVenenoCuandoLePreguntoSusPuntosDeVidaResponde0()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.subOnePoisonPoint();
+            Assert.AreEqual(0, sut.getPoisonPoints());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardConPlayerNameFooCuandoLePreguntoPorSuPlayerNameRespondeFoo()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            Assert.AreEqual("Foo", sut.getPlayerName());
+        }
+
+        [TestMethod]
+        public void TestDadoUnPlayerScoreboardConPlayerNameAlQueLeCambioElNombrePorPepeCuandoLePreguntoPorSuPlayerNameRespondeFoo()
+        {
+            PlayerScoreboard sut = new PlayerScoreboard();
+            sut.changeName("Pepe");
+            Assert.AreEqual("Pepe", sut.getPlayerName());
+        }
+
+
+    }
+}
