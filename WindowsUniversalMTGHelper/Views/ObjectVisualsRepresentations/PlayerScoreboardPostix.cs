5e65008cf2c0f88c6fcf45d35f3b43ef36d57bef
Lucas Alan Silvestri <silvestrilucas@outlook.com>
2016-06-14 13:20:58 -0300

    Refactor grafico en la vista(Mobile)

--- WindowsUniversalMTGHelper/Views/ObjectVisualsRepresentations/PlayerScoreboardPostix.cs
+++ WindowsUniversalMTGHelper/Views/ObjectVisualsRepresentations/PlayerScoreboardPostix.cs
@@ -5,6 +5,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Shapes;
 using WindowsUniversalMTGHelper.AppModel;
 using WindowsUniversalMTGHelper.Model;
@@ -19,6 +20,7 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
         private TextBlock playerLifePointsNumberTextBlock;
         private TextBlock playerPoisonPointNumberTextBlock;
         private Rectangle color;
+        private TextBox playerNameTextBox;
 
         public PlayerScoreboardPostix(PlayerScoreboardAppModel appModel)
         {
@@ -33,7 +35,7 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
             this.myCanvas = new Canvas();
             this.myCanvas.Width = 250;
             this.myCanvas.Height = 150;
-            this.myCanvas.Margin = new Thickness(0, 0, 0, 20);
+            this.myCanvas.Margin = new Thickness(0, 20, 0, 0);
             this.myCanvas.DataContext = this.owner;
 
             this.color = new Rectangle();
@@ -44,28 +46,30 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
             this.color.Height = 150;
             this.myCanvas.Children.Add(color);
 
-            TextBox playerNameTextBox = new TextBox();
-            playerNameTextBox.FontSize = 20;
-            playerNameTextBox.BorderThickness = new Thickness(0, 0, 0, 0);
-            playerNameTextBox.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
-            playerNameTextBox.HorizontalAlignment = HorizontalAlignment.Center;
-            playerNameTextBox.Margin = new Thickness(5, 0, 0, 0);
+            this.playerNameTextBox = new TextBox();
+            this.playerNameTextBox.FontSize = 20;
+            this.playerNameTextBox.MaxLength = 18;
+            this.playerNameTextBox.BorderThickness = new Thickness(0, 0, 0, 0);
+            this.playerNameTextBox.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            this.playerNameTextBox.HorizontalAlignment = HorizontalAlignment.Center;
+            this.playerNameTextBox.Margin = new Thickness(5, 0, 0, 0);
+            this.playerNameTextBox.TextChanged += new TextChangedEventHandler(ChangePositionOnRename);
             Binding bindingName = new Binding();
             bindingName.Path = new PropertyPath("playerName");
             bindingName.Source = this.owner;
-            BindingOperations.SetBinding(playerNameTextBox, TextBox.TextProperty, bindingName);
+            BindingOperations.SetBinding(this.playerNameTextBox, TextBox.TextProperty, bindingName);
 
             TextBlock playerLifePointsTextBlock = new TextBlock();
-            playerLifePointsTextBlock.FontSize = 20;
+            playerLifePointsTextBlock.FontSize = 16;
             playerLifePointsTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
             playerLifePointsTextBlock.Margin = new Thickness(15, 0, 0, 0);
             playerLifePointsTextBlock.Text = "LIFE POINTS: ";
 
             TextBlock playerPoisonPointsTextBlock = new TextBlock();
-            playerPoisonPointsTextBlock.FontSize = 20;
+            playerPoisonPointsTextBlock.FontSize = 16;
             playerPoisonPointsTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
             playerPoisonPointsTextBlock.Margin = new Thickness(15, 0, 0, 0);
-            playerPoisonPointsTextBlock.Text = "POISON POINTS: ";
+            playerPoisonPointsTextBlock.Text = "POISON COUNTERS: ";
 
             this.playerLifePointsNumberTextBlock = new TextBlock();
             this.playerLifePointsNumberTextBlock.FontSize = 20;
@@ -95,7 +99,7 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
             ppTextBlock.FontSize = 20;
             ppTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
             ppTextBlock.Margin = new Thickness(15, 0, 0, 0);
-            ppTextBlock.Text = "PP: ";
+            ppTextBlock.Text = "PC: ";
 
             Button plusOneLpButton = new Button();
             plusOneLpButton.Name = "+1";
@@ -156,22 +160,22 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
             changeColorButton.Click += new RoutedEventHandler(changeColorButtonCLick);
 
             Canvas.SetTop(playerNameTextBox, 0);
-            Canvas.SetLeft(playerNameTextBox, 0);
+            Canvas.SetLeft(playerNameTextBox, 105 + (this.playerNameTextBox.Text.Count() * (-4.4)));
             this.myCanvas.Children.Add(playerNameTextBox);
 
-            Canvas.SetTop(playerLifePointsTextBlock, 25);
+            Canvas.SetTop(playerLifePointsTextBlock, 30);
             Canvas.SetLeft(playerLifePointsTextBlock, 0);
             this.myCanvas.Children.Add(playerLifePointsTextBlock);
 
-            Canvas.SetTop(playerLifePointsNumberTextBlock, 25);
-            Canvas.SetLeft(playerLifePointsNumberTextBlock, 120);
+            Canvas.SetTop(playerLifePointsNumberTextBlock, 26);
+            Canvas.SetLeft(playerLifePointsNumberTextBlock, 90);
             this.myCanvas.Children.Add(playerLifePointsNumberTextBlock);
 
-            Canvas.SetTop(playerPoisonPointNumberTextBlock, 45);
-            Canvas.SetLeft(playerPoisonPointNumberTextBlock, 155);
+            Canvas.SetTop(playerPoisonPointNumberTextBlock, 46);
+            Canvas.SetLeft(playerPoisonPointNumberTextBlock, 145);
             this.myCanvas.Children.Add(playerPoisonPointNumberTextBlock);
 
-            Canvas.SetTop(playerPoisonPointsTextBlock, 45);
+            Canvas.SetTop(playerPoisonPointsTextBlock, 50);
             Canvas.SetLeft(playerPoisonPointsTextBlock, 0);
             this.myCanvas.Children.Add(playerPoisonPointsTextBlock);
 
@@ -179,48 +183,48 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
             Canvas.SetLeft(lpTextBlock, 0);
             this.myCanvas.Children.Add(lpTextBlock);
 
-            Canvas.SetTop(plusOneLpButton, 75);
-            Canvas.SetLeft(plusOneLpButton, 30);
-            this.myCanvas.Children.Add(plusOneLpButton);
+            Canvas.SetTop(subFiveLpButton, 75);
+            Canvas.SetLeft(subFiveLpButton, 30);
+            this.myCanvas.Children.Add(subFiveLpButton);
 
             Canvas.SetTop(subOneLpButton, 75);
-            Canvas.SetLeft(subOneLpButton, 75);
+            Canvas.SetLeft(subOneLpButton, 71);
             this.myCanvas.Children.Add(subOneLpButton);
 
-            Canvas.SetTop(plusFiveLpButton, 75);
-            Canvas.SetLeft(plusFiveLpButton, 120);
-            this.myCanvas.Children.Add(plusFiveLpButton);
+            Canvas.SetTop(plusOneLpButton, 75);
+            Canvas.SetLeft(plusOneLpButton, 120);
+            this.myCanvas.Children.Add(plusOneLpButton);
 
-            Canvas.SetTop(subFiveLpButton, 75);
-            Canvas.SetLeft(subFiveLpButton, 165);
-            this.myCanvas.Children.Add(subFiveLpButton);
+            Canvas.SetTop(plusFiveLpButton, 75);
+            Canvas.SetLeft(plusFiveLpButton, 165);
+            this.myCanvas.Children.Add(plusFiveLpButton);
 
             Canvas.SetTop(ppTextBlock, 112);
             Canvas.SetLeft(ppTextBlock, 0);
             this.myCanvas.Children.Add(ppTextBlock);
 
-            Canvas.SetTop(plusOnePpButton, 112);
-            Canvas.SetLeft(plusOnePpButton, 30);
-            this.myCanvas.Children.Add(plusOnePpButton);
-
+            Canvas.SetTop(subFivePpButton, 112);
+            Canvas.SetLeft(subFivePpButton, 30);
+            this.myCanvas.Children.Add(subFivePpButton);
+            
             Canvas.SetTop(subOnePpButton, 112);
-            Canvas.SetLeft(subOnePpButton, 75);
+            Canvas.SetLeft(subOnePpButton, 71);
             this.myCanvas.Children.Add(subOnePpButton);
 
-            Canvas.SetTop(plusFivePpButton, 112);
-            Canvas.SetLeft(plusFivePpButton, 120);
-            this.myCanvas.Children.Add(plusFivePpButton);
+            Canvas.SetTop(plusOnePpButton, 112);
+            Canvas.SetLeft(plusOnePpButton, 120);
+            this.myCanvas.Children.Add(plusOnePpButton);
 
-            Canvas.SetTop(subFivePpButton, 112);
-            Canvas.SetLeft(subFivePpButton, 165);
-            this.myCanvas.Children.Add(subFivePpButton);
+            Canvas.SetTop(plusFivePpButton, 112);
+            Canvas.SetLeft(plusFivePpButton, 165);
+            this.myCanvas.Children.Add(plusFivePpButton);
 
             Canvas.SetTop(deleteButton, 0);
             Canvas.SetLeft(deleteButton, 217);
             this.myCanvas.Children.Add(deleteButton);
 
             Canvas.SetTop(changeColorButton, 0);
-            Canvas.SetLeft(changeColorButton, 187);
+            Canvas.SetLeft(changeColorButton, 4);
             this.myCanvas.Children.Add(changeColorButton);
         }
 
@@ -229,21 +233,25 @@ namespace WindowsUniversalMTGHelper.Views.ObjectVisualsRepresentations
             List < SolidColorBrush > colors = new List<SolidColorBrush>();
             colors.Add(new SolidColorBrush(Windows.UI.Colors.Beige));
             colors.Add(new SolidColorBrush(Windows.UI.Colors.Chocolate));
-            colors.Add(new SolidColorBrush(Windows.UI.Colors.Cyan));
+            colors.Add(new SolidColorBrush(Windows.UI.Colors.DarkCyan));
             colors.Add(new SolidColorBrush(Windows.UI.Colors.DarkSalmon));
-            colors.Add(new SolidColorBrush(Windows.UI.Colors.Blue));
-            colors.Add(new SolidColorBrush(Windows.UI.Colors.GreenYellow));
+            colors.Add(new SolidColorBrush(Windows.UI.Colors.Aquamarine));
             colors.Add(new SolidColorBrush(Windows.UI.Colors.Gray));
             colors.Add(new SolidColorBrush(Windows.UI.Colors.Honeydew));
-            colors.Add(new SolidColorBrush(Windows.UI.Colors.OrangeRed));
             colors.Add(new SolidColorBrush(Windows.UI.Colors.LemonChiffon));
             colors.Add(new SolidColorBrush(Windows.UI.Colors.MistyRose));
+            colors.Add(new SolidColorBrush(Windows.UI.Colors.MediumSeaGreen));
             return colors.ElementAt(new Random().Next(0, colors.Count()-1));
         }
 
         public Canvas getShape()
         {
             return this.myCanvas;
+        }
+
+        public void ChangePositionOnRename(object sender, RoutedEventArgs e)
+        {
+            Canvas.SetLeft(playerNameTextBox, 105 + (playerNameTextBox.Text.Count() * (-4.4)));
         }
 
         private void addOneLifePointsButtonCLick(object sender, RoutedEventArgs e)
